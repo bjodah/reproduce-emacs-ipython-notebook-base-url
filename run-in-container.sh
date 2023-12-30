566b3aeb802d15053c5ea334c36047e0078381d2
Björn Ingvar Dahlgren <bjodah@gmail.com>
2023-12-30 10:54:43 +0100

    False alarm, ein seems to work fine with latest notebook-6.x

--- run-in-container.sh
+++ run-in-container.sh
@@ -7,8 +7,31 @@ read -p "Enter password (may be empty): " PASSWORD
 if [[ "$PASSWORD" == "" ]]; then
     NOTEBOOK_ARGS="--NotebookApp.token=''"
 else
+    if ! python3 -c "from notebook.auth import passwd" >/dev/null; then
+        echo "We need to generate a password hash using notebook-6.x, but it is unavailable"
+        echo "a throwaway virtualenv will be set up to generate the hash. Pass an empty password"
+        echo "to circumvent this, (or install notebook-6.x into the base env of 'python3')"
+        read -p "OK to proceed? [Y/n]" OK_TO_PROCEED
+        if [[ $OK_TO_PROCEED == n || $OK_TO_PROCEED == N ]]; then
+            exit 0
+        fi
+        if ! which virtualenv >/dev/null; then
+            >&2 echo "The command 'virtualenv' is not on path, aborting."
+            exit 1
+        fi
+        TEMP_ENV_DIR=$(mktemp -d)
+        cleanup() {
+            rm -r "$TEMP_ENV_DIR"
+        }
+        trap cleanup TERM EXIT
+        virtualenv $TEMP_ENV_DIR
+        $TEMP_ENV_DIR/bin/pip install 'notebook<7';
+        PASSWORD_SHA1=$($TEMP_ENV_DIR/bin/python -c "from notebook.auth import passwd; print(passwd(\"$PASSWORD\", algorithm='sha1'), end='')")
+        echo $PASSWORD_SHA1
+    else
+        PASSWORD_SHA1=$(python3 -c "from notebook.auth import passwd; print(passwd(\"$PASSWORD\", algorithm='sha1'), end='')")
+    fi
     set -x
-    PASSWORD_SHA1=$(python3 -c "from notebook.auth import passwd; print(passwd(\"$PASSWORD\", algorithm='sha1'), end='')")
     NOTEBOOK_ARGS="--NotebookApp.token='' --NotebookApp.password=\"${PASSWORD_SHA1}\""
 fi
 
